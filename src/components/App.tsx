31a96d18377b8afec1aac818edddd88132199181
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-06-09 10:21:04 -0400

    Added stats graph

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
 import './App.scss';
 import { Header } from './header/Header';
 import { Picture } from './picture/Picture';
+import { Statistics } from './statistics/Statistics';
 
 export const App = (): JSX.Element => {
 	return (
 		<div className="dashboard">
 			<Header />
-			<Picture />
+			<div className="dashboard__section-1">
+				<Picture />
+				<Statistics />
+			</div>
 		</div>
 	);
 };
---
+++ src/components/statistics/Statistics.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
+import { useAppSelector } from '../../util/hooks';
+import './Statistics.scss';
+
+export type StatisticsProps = {};
+
+type PokeStats = {
+	base_stat: number;
+	effort: number;
+	stat: {
+		name: string;
+		url: string;
+	};
+};
+
+export const Statistics = (props: StatisticsProps) => {
+	const pokemonDetails: any = useAppSelector((state) => {
+		return state.pokemonDetails;
+	});
+	const [pokeStats, setPokeStats] = useState<PokeStats[]>([]);
+
+	useEffect(() => {
+		if (pokemonDetails) {
+			setPokeStats(pokemonDetails.stats);
+		}
+	}, [pokemonDetails]);
+
+	const graph = (): JSX.Element[] => {
+		const result: JSX.Element[] = [];
+		for (let i = 0; i < pokeStats.length; i++) {
+			result.push(
+				<div
+					key={i}
+					style={{ gridColumn: `1 / ${pokeStats[i].base_stat}` }}
+					className={`stats__graph-${i}`}
+				></div>
+			);
+		}
+		return result;
+	};
+
+	return (
+		<div className="stats">
+			<div className="stats__header">Stats</div>
+			<div className="stats__name">
+				<p>Hp</p>
+				<p>Attack</p>
+				<p>Defense</p>
+				<p>Sp. Attack</p>
+				<p>Sp. Defense</p>
+				<p>Speed</p>
+			</div>
+			<div className="stats__graph">{graph()}</div>
+		</div>
+	);
+};
