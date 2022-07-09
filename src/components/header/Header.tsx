1c42296b33c925a32d5f484fc8d66d2fea11934f
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-07-09 17:23:07 -0400

    Adding pokemon logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,13 @@
 import { PokeDropDown } from '../dropdown/PokeDropDown';
+import pokemonLogo from 'img/pokemon-logo/pokemon-logo-text.png';
 import './Header.scss';
 
 export const Header = () => {
 	return (
 		<div className="header">
+			<div className="header-container">
+				<img className="header-logo" src={pokemonLogo} alt="Pokemon Logo" />
+			</div>
 			<PokeDropDown />
 		</div>
 	);
