cb96dfe5688db32ba7b54804a38bc73304542523
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-07-23 22:19:43 -0400

    English text for every pokemon

--- src/components/message/PokemonMessage.tsx
+++ src/components/message/PokemonMessage.tsx
@@ -2,6 +2,18 @@ import { useEffect, useState } from 'react';
 import { useAppSelector } from 'util/hooks';
 import { Message } from './Message';
 
+type FlavorTextEntry = {
+	flavor_text: string;
+	language: {
+		name: string;
+		url: string;
+	};
+	version: {
+		name: string;
+		url: string;
+	};
+};
+
 export const PokemonMessage = () => {
 	const pokemonSpecies: any = useAppSelector((state) => {
 		return state.pokemonSpecies;
@@ -9,9 +21,22 @@ export const PokemonMessage = () => {
 
 	const [message, setMessage] = useState<string>('');
 
+	const getEnglishText = (): string | null => {
+		const ENGLISH_TEXT: string = 'en';
+		const result: FlavorTextEntry = pokemonSpecies.flavor_text_entries.find(
+			(obj: FlavorTextEntry) => obj.language.name === ENGLISH_TEXT
+		);
+		return result.flavor_text;
+	};
+
 	useEffect(() => {
 		if (pokemonSpecies) {
-			setMessage(pokemonSpecies.flavor_text_entries[0].flavor_text);
+			const message = getEnglishText();
+			if (message) {
+				setMessage(message);
+			} else {
+				setMessage('No English Message Available');
+			}
 		}
 	}, [pokemonSpecies]);
 
